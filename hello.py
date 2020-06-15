c3d84c600e2212c9fa325998f31fc1d16572b925
Khushi-Shukla <tusharika.shukla05@gmail.com>
2020-06-15 14:23:40 +0530

    changed

--- hello.py
+++ hello.py
@@ -1,13 +1,3 @@
 print('Hello, world!')
 print('in dev branch')
 print('in dev2 branch')
-def isMonotonic(A): 
-  
-    return (all(A[i] <= A[i + 1] for i in range(len(A) - 1)) or
-            all(A[i] >= A[i + 1] for i in range(len(A) - 1))) 
-  
-# Driver program 
-A = [6, 5, 4, 4] 
-  
-# Print required result 
-print(isMonotonic(A)) 
